6f07ea418027f9a9e8ffe0ce50dd1bc00457340c
feather2308 <feather2308@naver.com>
2023-11-08 10:45:20 +0900

    commit and push

--- src/base/helloworld.java
+++ src/base/helloworld.java
@@ -1,12 +1,9 @@
 package base;
 
 public class helloworld {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("hello world!");
 		System.out.println("Hi");
 		System.out.println("bye");
 	}
-
 }
