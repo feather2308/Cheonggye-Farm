8af43bc4ca45937c3eeeb6ceacecbf6c403a44cd
feather2308 <feather2308@naver.com>
2023-11-08 14:15:07 +0900

    font 인풋스트림 수정

---
+++ src/base/MyFarm.java
@@ -0,0 +1,43 @@
+package base;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MyFarm extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MyFarm frame = new MyFarm(new int[] {100, 100, 80});
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MyFarm(int[] bound) {
+		setTitle("청계 농장");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(bound[0], bound[1], 16*bound[2], 9*bound[2]);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
---
+++ src/base/MyLauncher.java
@@ -0,0 +1,102 @@
+package base;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JComboBox;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.io.IOException;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.imageio.ImageIO;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.FontFormatException;
+
+public class MyLauncher extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MyLauncher frame = new MyLauncher();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MyLauncher() {
+		Font font = createFont();
+		
+		setResizable(false);
+		setTitle("청계 농장");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("로고 라벨");
+		lblNewLabel.setFont(font);
+		lblNewLabel.setBounds(12, 10, 52, 15);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("해상도");
+		lblNewLabel_1.setFont(font);
+		lblNewLabel_1.setBounds(284, 196, 36, 15);
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblNewLabel_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(font);
+		comboBox.setBounds(332, 191, 90, 25);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"960x540", "1280x720", "1600x900", "1920x1080", "2560x1440"}));
+		comboBox.setSelectedIndex(1);
+		comboBox.setToolTipText("");
+		contentPane.add(comboBox);
+		
+		JButton btnNewButton = new JButton("시작");
+		btnNewButton.setFont(font);
+		btnNewButton.setBounds(260, 226, 75, 25);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("종료");
+		btnNewButton_1.setFont(font);
+		btnNewButton_1.setBounds(347, 226, 75, 25);
+		contentPane.add(btnNewButton_1);
+	}
+	
+	private Font createFont() {
+	    try {
+	        Font customFont = Font.createFont(Font.TRUETYPE_FONT, getClass().getResourceAsStream("/resource/MAPLESTORY BOLD.TTF"));
+	        customFont = customFont.deriveFont(12f);
+	        return customFont;
+	    } catch (IOException | FontFormatException e) {
+	        System.err.println("폰트를 불러올 수 없습니다: " + e.getMessage());
+	        return new Font("굴림", Font.PLAIN, 12);
+	    }
+	}
+
+}
